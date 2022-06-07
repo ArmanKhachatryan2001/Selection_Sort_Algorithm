77697fb769c90daeb7e9fb6bda5e119f73484ac1
ArmanKhachatryan2001 <104670251+ArmanKhachatryan2001@users.noreply.github.com>
2022-06-07 16:48:11 +0400

    Update selection_sort.cpp

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,42 +1,30 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
+
 template <typename iterator>
-void selection_sort(iterator begin, iterator end)
+void selection_sort(iterator begin, iterator end) // with iterators
 {
     for (iterator i = begin; i != end; ++i) {
-    std::iter_swap(i, std::min_element(i, end));
+        std::iter_swap(i, std::min_element(i, end));
     }
 }
 template <typename T>
-void sort(T* arr, int size)
+void selection_sort(T* arr, int size) // implemented for a simple array
 {
     int min{};
     for(int i = 0; i < size - 1; ++i) {
-    min = i;
-    for(int j = i + 1; j < size; ++j) {
-    if(arr[j] < arr[min]) {
-    min = j;
-    }
-    }
-    T my_swap = arr[min];
-    arr[min] = arr[i];
-    arr[i] = my_swap;
+        min = i;
+        for(int j = i + 1; j < size; ++j) {
+            if(arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        T my_swap = arr[min];
+        arr[min] = arr[i];
+        arr[i] = my_swap;
     }
 }
 
 int main()
 {
- std::vector<int> m = {9,5,7,3};
-    selection_sort(m.begin(), m.end());
-    for(auto i : m)
-    std::cout << i << " ";
-    std::cout << std::endl;
-    int arr[5] = {6,2,3,9,0};
-    sort(arr, 5);
-    for(int i = 0; i < 5; ++i) {
-        std::cout << arr[i] << " ";
-    }
-    return 0;
 }
-
